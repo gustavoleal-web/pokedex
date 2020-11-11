6c4c24720efcece3c264f2ccdac433d36a5fb386
gustavoleal-web <74203527+gustavoleal-web@users.noreply.github.com>
2020-11-11 15:19:44 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
-const genOnePokedex = async ( name ) => {
+const colorTypes = ( typeOne, typeTwo ) => {
+    const type1 = typeOne.innerText.trim();
+    typeOne.classList.add( type1 )
+
+//not all pokemons have 2 types so it's only created if it exists
+    if ( typeTwo.innerText ) {
+        const type2 = typeTwo.innerText.trim()
+        typeTwo.classList.add( type2 )
+    }
+   
+
+}
+
+const getAbilities = ( div, img, uniquePkm ) => {
+    const { abilities } = uniquePkm;
+    const abilitiesTitle = document.createElement( 'h4' );
+    const basicAbility = document.createElement( 'p' );
+    const hiddenAbility = document.createElement( 'p' );
+
+    abilitiesTitle.textContent = 'Abilities';
+    div.appendChild( abilitiesTitle );
+
+    img.addEventListener( 'click', ( e ) => {
+        //so duplicate abilities aren't added to the div every time the div is clicked
+        if ( basicAbility.textContent === '' ) {
+            for ( let i = 0; i < abilities.length; i++ ) {
+                if ( abilities[ i ].is_hidden ) {
+                    hiddenAbility.textContent = `Hidden: ${ abilities[ i ].ability.name }`;
+                }
+
+                if ( !abilities[ i ].is_hidden ) {
+                    basicAbility.textContent = ` ${ abilities[ i ].ability.name }`;
+                }
+
+            }
+        }
+        div.appendChild( basicAbility );
+        div.appendChild( hiddenAbility );
+    } )
+}
+
+
+const getPokedex = async ( name = 'charmander' ) => {
     let res;
     let uniquePkm;
     try {
@@ -14,14 +56,27 @@ const genOnePokedex = async ( name ) => {
     let img = document.createElement( 'img' );
     let h3 = document.createElement( 'h3' );
     let container = document.querySelector( '.container' );
-    let p = document.createElement( 'p' );
-
+    let typeOne = document.createElement( 'p' );
+    let typeTwo = document.createElement( 'p' );
     let pokTypes = uniquePkm.types;
 
-
-    for ( let i = 0; i < pokTypes.length; i++ ) {
-        p.innerText += `${ pokTypes[ i ].type.name } `;
+    if ( pokTypes.length === 2 ) {
+        typeOne.innerText = `${ pokTypes[ 0 ].type.name } `;
+        typeTwo.innerText = `${ pokTypes[ 1 ].type.name } `;
+    } else {
+        typeOne.innerText = `${ pokTypes[ 0 ].type.name } `;
     }
+
+    colorTypes( typeOne, typeTwo );
+
+
+    // for ( let i = 0; i < pokTypes.length; i++ ) {
+    //     p.innerText += `${ pokTypes[ i ].type.name } `;
+    //     if ( pokTypes[ i ].type.name === 'fire' ) {
+    //         p.classList.add( 'fire' )
+    //     }
+    // }
+
 
     img.src = uniquePkm.sprites.other.dream_world.front_default;
     h3.innerText = name;
@@ -29,37 +84,22 @@ const genOnePokedex = async ( name ) => {
     container.appendChild( div );
     div.appendChild( img );
     div.appendChild( h3 );
-    div.appendChild( p );
+    div.appendChild( typeOne );
+    div.appendChild( typeTwo );
     div.classList.add( 'border' );
 
+    getAbilities( div, img, uniquePkm )
 
 
     // const pokemonInfo = () => {
-
     //     div.addEventListener( 'click', ( e ) => {
-    //         console.log(uniquePkm.moves)
+    //         console.log( uniquePkm.moves[ 0 ].version_group_details)
     //     } );
     // }
 
     // pokemonInfo();
 }
 
-// const genOne = async () => {
-//     try {
-//         let res = await fetch( `https://pokeapi.co/api/v2/pokedex/kanto` );
-//         let pokemon = await res.json();
-
-//         pokemon.pokemon_entries.forEach( p => {
-//             genOnePokedex( p.entry_number )
-//         } )
-//     }
-//     catch ( e ) {
-//         console.log( e )
-//     }
-
-// }
-
-// genOne();
 
 const getOtherGens = async ( generation ) => {
     try {
@@ -70,7 +110,7 @@ const getOtherGens = async ( generation ) => {
             if ( p.pokemon_species.name === 'deoxys' ) {
                 return;
             }
-            genOnePokedex( p.pokemon_species.name )
+            getPokedex( p.pokemon_species.name )
         } )
     }
     catch ( e ) {
@@ -79,7 +119,9 @@ const getOtherGens = async ( generation ) => {
 
 }
 
-getOtherGens( 'kanto' );
+getPokedex()
+
+//getOtherGens( 'kanto' );
 
 const kanto = document.querySelector( '.kanto' );
 kanto.addEventListener( 'click', ( e ) => events( e ) );
@@ -92,7 +134,6 @@ const events = ( e ) => {
         container.removeChild( container.lastChild );
     }
     getOtherGens( e.target.value );
-    console.log( e.target.value )
 }
 
 const hoenn = document.querySelector( '.hoenn' );
